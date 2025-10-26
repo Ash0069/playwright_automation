4c673ca221f63cc0916e18de82a9b77309d4a073
Nishant Agrawal <nishantagrawal2003@gmail.com>
2025-10-27 02:48:58 +0530

    playwright config file

---
+++ playwright.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  retries: 1,
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    { name: 'Chromium', use: { ...devices['Desktop Chrome'] } }
+  ],
+});
